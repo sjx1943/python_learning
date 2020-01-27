f3e58488b2e762c45a8041e0fb35e7a433f9111c
sgg <sjx.1943@163.com>
2020-01-27 21:42:58 +0800

    i mean upload
    Signed-off-by: sjx1943 <sjx.1943@163.com>

--- practice/test.py
+++ practice/test.py
@@ -37,20 +37,32 @@ class Solution:
 
 
     def f(self,prices):
-        minprice = float('inf')
-        maxprofit = 0
-        for i in range(len(prices)):
+        k_max=2
+        n = len(prices)
+        if n==0:
+            return 0
+        dp = [[[0,0],[0,0],[0,0]] for _ in range(n)]
+        # dp = [[[0, 0, 0], [0,0,0]] for _ in range(n)]
+        # dp[-1][k_max][1] = -float('inf')
+        # dp[-1][k_max][0] = 0
+        for i in range(n):
+            for k in range(k_max,0,-1):
+                if i-1==-1:
+                    dp[i][k][0]=0
+                    dp[i][k][1]=-prices[0]
+                    continue
+                dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i])
+                dp[i][k][1] = max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i])
 
-            if prices[i]<minprice:
-                minprice = prices[i]
-            elif prices[i] - minprice>maxprofit:
 
-                maxprofit = prices[i]-minprice
-        return maxprofit
+            # dp[i][0] = max(dp[i-1][0],dp[i-1][1]+prices[i])
+            # dp[i][1] = max(dp[i - 1][1], dp[i-1][0]-prices[i])
+
+        return dp[n-1][k_max][0]
 
 
-#
 S = Solution()
-print(S.f([7,1,5,3,6,4]))
+print(S.f([3,3,5,0,0,3,1,4]))
 
 # Research environment functions
+
